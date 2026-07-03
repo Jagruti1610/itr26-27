abdfe7b2c822d87fc947255eb360c59466bf3daf
Jagruti1610 <jahire932@gmail.com>
2026-07-03 17:46:36 +0530

    database connectivity

---
+++ python/hello.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+print("hello")
+print("mi jagruti ahe")
+print("mi thodi yedi ye pn ky kru mazi maitrin ahe hushar mhanun mala mi hushar vatte")
+print("how are you gauri")
+print("gauri ronak changle bahin bhau ahe ani dogh hushar ahe")
